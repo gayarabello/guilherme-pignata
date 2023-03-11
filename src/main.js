b9b95139356852464a3b16590ea9116d68a37670
gayarabello <gayarabello@gmail.com>
2023-03-11 18:55:30 +0100

    add routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import HomePage from './pages/HomePage.vue'
+import ServicePage from './pages/ServicePage.vue'
+import InitialConsultationPage from './pages/InitialConsultationPage.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomePage },
+    { path: '/service/:id', component: ServicePage },
+    { path: '/initial-consultation', component: InitialConsultationPage },
+  ]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes, 
+  })
+
+  const app = createApp(App)
+  app.use(router)
+  app.mount('#app')
