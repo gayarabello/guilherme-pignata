9e1133548f943fd74278d809c201cc3b13adc004
gayarabello <gayarabello@gmail.com>
2023-03-11 18:55:55 +0100

    add data

---
+++ src/data/plans.js
@@ -0,0 +1,98 @@
+export default {
+  'health-coach': [
+    {
+      title: "Basic",
+      price: 400,
+      period: "3",
+      planPoints: [
+        `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+        `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+        `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+        `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+        `Ongoing food diary evaluation`,
+        `Weekly habit tracking`,
+        `On-going body composition assessment / Progress tracking`,
+        `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+      ],
+    },
+    {
+      title: "Full",
+      price: 600,
+      period: "6",
+      planPoints: [
+          `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+          `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+          `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+          `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+          `Ongoing food diary evaluation`,
+          `Weekly habit tracking`,
+          `On-going body composition assessment / Progress tracking`,
+          `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+        ],
+    },
+  ],
+  'personal-training': [
+    {
+      title: "Basic",
+      price: 400,
+      period: "3",
+      planPoints: [
+        `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+        `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+        `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+        `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+        `Ongoing food diary evaluation`,
+        `Weekly habit tracking`,
+        `On-going body composition assessment / Progress tracking`,
+        `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+      ],
+    },
+    {
+      title: "Full",
+      price: 600,
+      period: "6",
+      planPoints: [
+          `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+          `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+          `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+          `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+          `Ongoing food diary evaluation`,
+          `Weekly habit tracking`,
+          `On-going body composition assessment / Progress tracking`,
+          `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+        ],
+    },
+  ],
+  'jiu-jitsu': [
+    {
+      title: "Basic",
+      price: 400,
+      period: "3",
+      planPoints: [
+        `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+        `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+        `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+        `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+        `Ongoing food diary evaluation`,
+        `Weekly habit tracking`,
+        `On-going body composition assessment / Progress tracking`,
+        `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+      ],
+    },
+    {
+      title: "Full",
+      price: 600,
+      period: "6",
+      planPoints: [
+          `One 45-60 minute online consultation a month for general habits (eating, moving, sleeping) advice and suggestions, evaluate progress, and identify new areas for development.`,
+          `Exercise plan periodised monthly with program delivered weekly straight to your phone with video demos and descriptions, and progress tracking: you train wherever and whenever you want, and stay on track`,
+          `One 45-60 min personal training session a month for exercise demonstration, adjustments and updates`,
+          `Four 10-12 min calls a month (one a week) for Q&amp;A and followup.`,
+          `Ongoing food diary evaluation`,
+          `Weekly habit tracking`,
+          `On-going body composition assessment / Progress tracking`,
+          `3-months assessment online call (45-60min) for analysing progresses and defining next steps - should happen half way on the 3rd month`,
+        ],
+    },
+  ]
+}
---
+++ src/data/services.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: "health-coach",
+    title: "HEALTH COACH",
+    description:
+      "Some of the leading causes of death in the western world can be prevented, stoped or even reversed just by implementing good dietary habits and regular exercise into your daily life.",
+    action: "See More",
+  },
+  {
+    id: "personal-training",
+    title: "PERSONAL TRAINING",
+    description:
+    `Regular exercise has numerous benefits, including improving cardiovascular health, building strength and endurance, reducing the risk of chronic diseases such as diabetes and obesity, and enhancing mental health by reducing stress and anxiety.
+    <br>
+    <br>
+    Engaging in physical activity also helps to maintain a healthy body weight and improve overall quality of life.
+    `,
+    action: "See More",
+  },
+  {
+    id: "jiu-jitsu",
+    title: "BRAZILIAN JIU JITSU/SELF DEFENCE",
+    description:
+      `BJJ is a non-striking martial art that provides a full-body workout improving cardiovascular health, building strength and flexibility, and increasing overall physical fitness.
+      <br>
+      <br>
+      It enhances mental focus and discipline and reduces stress and anxiety, boosting confidence and self-esteem.
+      <br>
+      <br>
+      It can also help improve balance and coordination, which can be especially beneficial for older adults or those with mobility issues.`,
+    action: "See More",
+  },
+];
